e01132c76e1ea2ab7aad333a5aa7107a5c4d5d9d
rask24 <70057885+rask24@users.noreply.github.com>
2023-09-28 21:30:17 +0900

    fix strlcat (#3)
    Co-authored-by: Reiya Asuke <reasuke@c1r24s7.42tokyo.jp>

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 17:37:54 by reasuke           #+#    #+#             */
-/*   Updated: 2023/09/24 15:42:14 by reasuke          ###   ########.fr       */
+/*   Updated: 2023/09/28 20:10:50 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,5 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		return (ft_strlen(src) + dstsize);
 	while (*dst)
 		dst++;
-	ft_strlcpy(dst, src, dstsize - dst_len);
-	return (ft_strlen(src) + dst_len);
+	return (ft_strlcpy(dst, src, dstsize - dst_len) + dst_len);
 }
