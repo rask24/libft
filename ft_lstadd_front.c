83ea7841b0b1d2b3a015d44e0fbbb50fcd0317e8
rask24 <rask0x18@gmail.com>
2023-09-29 22:44:39 +0900

    add null check

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 19:06:44 by reasuke           #+#    #+#             */
-/*   Updated: 2023/09/22 20:03:27 by reasuke          ###   ########.fr       */
+/*   Updated: 2023/09/29 22:41:49 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!lst)
+	if (!lst || !new)
 		return ;
 	if (!*lst)
 	{
