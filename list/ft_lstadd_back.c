73ed79dcf8cfdf617045b5509e65a354a5d0aa37
rask24 <rask0x18@gmail.com>
2023-11-09 17:48:39 +0900

    rename new to new_list on list

--- list/ft_lstadd_back.c
+++ list/ft_lstadd_back.c
@@ -6,20 +6,20 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 19:22:37 by reasuke           #+#    #+#             */
-/*   Updated: 2023/10/12 19:29:14 by reasuke          ###   ########.fr       */
+/*   Updated: 2023/11/09 15:35:01 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_lst)
 {
 	if (!lst)
 		return ;
 	if (!*lst)
 	{
-		*lst = new;
+		*lst = new_lst;
 		return ;
 	}
-	ft_lstlast(*lst)->next = new;
+	ft_lstlast(*lst)->next = new_lst;
 }
