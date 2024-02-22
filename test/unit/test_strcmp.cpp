0040bc4599063c6f5c9f1379cda960e951edcbc5
rask24 <70057885+rask24@users.noreply.github.com>
2024-02-22 13:35:12 +0900

    update test (#38)
    * update test with unit_test.mk
    
    * refactor test
    
    * fix ci
    
    * add integer test
    
    * fix ci
    
    * delete build

---
+++ test/unit/test_strcmp.cpp
@@ -0,0 +1,22 @@
+// Copyright 2024, reasuke.
+
+#include <string>
+
+#include "gtest/gtest.h"
+#include "unit_test_utils.hpp"
+
+extern "C" {
+#include <string.h>
+
+#include "libft.h"
+}
+
+TEST(ft_strcmp, test) {
+  std::string s1("Hello World");
+  std::string s2("Hello:World");
+
+  CHECK_EQ(strcmp, s1.c_str(), s1.c_str());
+  CHECK_EQ(strcmp, s2.c_str(), s2.c_str());
+  CHECK_EQ(strcmp, s1.c_str(), s2.c_str());
+  CHECK_EQ(strcmp, s2.c_str(), s1.c_str());
+}
