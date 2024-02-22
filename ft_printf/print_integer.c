aa8ad181d6a79e75fe28e780ee51e1ae6971bd78
rask24 <rask0x18@gmail.com>
2024-02-22 23:45:34 +0900

    update constant

--- ft_printf/print_integer.c
+++ ft_printf/print_integer.c
@@ -6,37 +6,36 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 18:12:55 by reasuke           #+#    #+#             */
-/*   Updated: 2023/10/17 15:43:08 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/22 23:42:41 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	fpf_init_width(
-				t_integer_info *info, intmax_t nb, t_format_spec *fs)
+static void	fpf_init_width(t_integer_info *info, intmax_t nb, t_format_spec *fs)
 {
 	info->prefix_width = ft_strlen(info->prefix);
 	if (nb == 0 && fs->precision == 0)
 		info->digits = 0;
 	else
-		info->digits = fpf_digits_base(
-				nb, ft_strlen(info->base), info->is_signed);
+		info->digits = fpf_digits_base(nb, ft_strlen(info->base),
+				info->is_signed);
 	if (fs->width && fs->precision == PREC_NONE && fs->flags & FLAG_ZERO)
 	{
 		info->space_width = 0;
-		info->zero_width = fpf_max(fs->width
-				- info->digits - info->prefix_width, 0);
+		info->zero_width = fpf_max(fs->width - info->digits
+				- info->prefix_width, 0);
 	}
 	else
 	{
-		info->space_width = fpf_max(fs->width
-				- fpf_max(fs->precision, info->digits) - info->prefix_width, 0);
+		info->space_width = fpf_max(fs->width - fpf_max(fs->precision,
+					info->digits) - info->prefix_width, 0);
 		info->zero_width = fpf_max(fs->precision - info->digits, 0);
 	}
 }
 
-static void	fpf_init_prefix(
-				t_integer_info *info, intmax_t nb, t_format_spec *fs)
+static void	fpf_init_prefix(t_integer_info *info, intmax_t nb,
+		t_format_spec *fs)
 {
 	info->prefix = "";
 	if (info->is_signed && nb < 0)
@@ -46,21 +45,21 @@ static void	fpf_init_prefix(
 	else if (info->is_signed && fs->flags & FLAG_SPACE)
 		info->prefix = " ";
 	else if (fs->flags & FLAG_HASH && fs->conversion == 'x' && nb)
-		info->prefix = HEX_PREFIX_LOWER;
+		info->prefix = LOEWR_HEX_PREFIX;
 	else if (fs->flags & FLAG_HASH && fs->conversion == 'X' && nb)
-		info->prefix = HEX_PREFIX_UPPER;
+		info->prefix = UPPER_HEX_PREFIX;
 	else if (fs->conversion == 'p')
-		info->prefix = HEX_PREFIX_LOWER;
+		info->prefix = LOEWR_HEX_PREFIX;
 }
 
-static void	fpf_init_integer_info(
-				t_integer_info *info, intmax_t nb, t_format_spec *fs)
+static void	fpf_init_integer_info(t_integer_info *info, intmax_t nb,
+		t_format_spec *fs)
 {
 	info->is_signed = (fs->conversion == 'd' || fs->conversion == 'i');
 	if (fs->conversion == 'x' || fs->conversion == 'p')
-		info->base = HEX_BASE_LOEWR;
+		info->base = LOWER_HEX_BASE;
 	else if (fs->conversion == 'X')
-		info->base = HEX_BASE_UPPER;
+		info->base = UPPER_HEX_BASE;
 	else if (fs->conversion == 'o')
 		info->base = OCT_BASE;
 	else
@@ -92,6 +91,6 @@ void	fpf_print_integer(intmax_t nb, t_format_spec *fs, t_format_result *fr)
 		fpf_putnbr_base(nb, info.base, info.is_signed);
 	if (fs->flags & FLAG_MINUS && info.space_width)
 		fpf_print_padding(' ', info.space_width);
-	fr->cnt += info.space_width
-		+ info.prefix_width + info.zero_width + info.digits;
+	fr->cnt += info.space_width + info.prefix_width + info.zero_width
+		+ info.digits;
 }
