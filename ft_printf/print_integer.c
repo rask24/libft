a9a94870b7304ad289fe5168718e869d3c9fbbff
rask24 <rask0x18@gmail.com>
2024-02-23 22:00:08 +0900

    rename print_integer

--- ft_printf/print_integer.c
+++ ft_printf/print_integer.c
@@ -6,13 +6,13 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 18:12:55 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/23 00:21:33 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/23 21:59:03 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	_init_width(t_integer_info *info, intmax_t nb, t_format_info *fi)
+static void	_set_width(t_integer_info *info, intmax_t nb, t_format_info *fi)
 {
 	info->prefix_width = ft_strlen(info->prefix);
 	if (nb == 0 && fi->precision == 0)
@@ -34,7 +34,7 @@ static void	_init_width(t_integer_info *info, intmax_t nb, t_format_info *fi)
 	}
 }
 
-static void	_init_prefix(t_integer_info *info, intmax_t nb, t_format_info *fi)
+static void	_set_prefix(t_integer_info *info, intmax_t nb, t_format_info *fi)
 {
 	info->prefix = "";
 	if (info->is_signed && nb < 0)
@@ -51,20 +51,9 @@ static void	_init_prefix(t_integer_info *info, intmax_t nb, t_format_info *fi)
 		info->prefix = LOEWR_HEX_PREFIX;
 }
 
-static void	_init_integer_info(t_integer_info *info, intmax_t nb,
-		t_format_info *fi)
+static void	_adjust_for_oct_prefix(t_integer_info *info, intmax_t nb,
+				t_format_info *fi)
 {
-	info->is_signed = (fi->conversion == 'd' || fi->conversion == 'i');
-	if (fi->conversion == 'x' || fi->conversion == 'p')
-		info->base = LOWER_HEX_BASE;
-	else if (fi->conversion == 'X')
-		info->base = UPPER_HEX_BASE;
-	else if (fi->conversion == 'o')
-		info->base = OCT_BASE;
-	else
-		info->base = DEC_BASE;
-	_init_prefix(info, nb, fi);
-	_init_width(info, nb, fi);
 	if (fi->flags & FLAG_HASH && !info->is_signed && fi->conversion == 'o'
 		&& !info->zero_width && (nb || fi->precision == 0))
 	{
@@ -75,11 +64,28 @@ static void	_init_integer_info(t_integer_info *info, intmax_t nb,
 	}
 }
 
+static void	_configure_integer_info(t_integer_info *info, intmax_t nb,
+				t_format_info *fi)
+{
+	info->is_signed = (fi->conversion == 'd' || fi->conversion == 'i');
+	if (fi->conversion == 'x' || fi->conversion == 'p')
+		info->base = LOWER_HEX_BASE;
+	else if (fi->conversion == 'X')
+		info->base = UPPER_HEX_BASE;
+	else if (fi->conversion == 'o')
+		info->base = OCT_BASE;
+	else
+		info->base = DEC_BASE;
+	_set_prefix(info, nb, fi);
+	_set_width(info, nb, fi);
+	_adjust_for_oct_prefix(info, nb, fi);
+}
+
 void	fpf_print_integer(intmax_t nb, t_format_info *fi, t_format_result *fr)
 {
 	t_integer_info	info;
 
-	_init_integer_info(&info, nb, fi);
+	_configure_integer_info(&info, nb, fi);
 	if (!(fi->flags & FLAG_MINUS) && info.space_width)
 		fpf_print_padding(' ', info.space_width);
 	if (*info.prefix)
