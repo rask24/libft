81231809d43848c4c39a4cf114dd6aa6772ce5f0
rask24 <rask0x18@gmail.com>
2024-02-23 23:46:15 +0900

    update print_padding

--- ft_printf/print_integer/print_integer.c
+++ ft_printf/print_integer/print_integer.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 18:12:55 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/23 22:46:08 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/23 23:44:17 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ void	fpf_print_integer(intmax_t nb, t_format_info *fi, t_format_result *fr)
 
 	fp_configure_integer_info(&info, nb, fi);
 	if (!(fi->flags & FLAG_MINUS) && info.space_width)
-		fp_print_padding(' ', info.space_width);
+		fp_print_padding(' ', info.space_width, STDOUT_FILENO);
 	if (*info.prefix)
 		ft_putstr_fd(info.prefix, STDOUT_FILENO);
 	if (info.zero_width)
-		fp_print_padding('0', info.zero_width);
+		fp_print_padding('0', info.zero_width, STDOUT_FILENO);
 	if (info.digits)
 		fp_putnbr_base(nb, info.base, info.is_signed);
 	if (fi->flags & FLAG_MINUS && info.space_width)
-		fp_print_padding(' ', info.space_width);
+		fp_print_padding(' ', info.space_width, STDOUT_FILENO);
 	fr->cnt += info.space_width + info.prefix_width + info.zero_width
 		+ info.digits;
 }
--- ft_printf/print_string/print_char.c
+++ ft_printf/print_string/print_char.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 17:02:59 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/23 22:45:48 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/23 23:44:31 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void	fp_print_char(char c, t_format_info *fi, t_format_result *fr)
 {
 	if (fi->width && !(fi->flags & FLAG_MINUS) && !(fi->flags & FLAG_ZERO))
-		fp_print_padding(' ', fi->width - 1);
+		fp_print_padding(' ', fi->width - 1, STDOUT_FILENO);
 	else if (fi->width && !(fi->flags & FLAG_MINUS) && fi->flags & FLAG_ZERO)
-		fp_print_padding('0', fi->width - 1);
+		fp_print_padding('0', fi->width - 1, STDOUT_FILENO);
 	ft_putchar_fd(c, STDOUT_FILENO);
 	if (fi->width && fi->flags & FLAG_MINUS)
-		fp_print_padding(' ', fi->width - 1);
+		fp_print_padding(' ', fi->width - 1, STDOUT_FILENO);
 	if (fi->width)
 		fr->cnt += fi->width;
 	else
