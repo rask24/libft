f5414d7a38d7f5aad02e55367c903b329d93b4f7
rask24 <rask0x18@gmail.com>
2024-02-23 23:26:46 +0900

    add ft_vdprintf

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 14:20:27 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/23 23:09:20 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/23 23:21:45 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,6 +88,7 @@ typedef struct s_integer_info
 
 int				ft_printf(const char *format, ...);
 int				ft_vprintf(const char *format, va_list ap);
+int				ft_vdprintf(int fd, const char *format, va_list ap);
 
 void			fp_configure_format_info( t_format_info *fi,
 					t_format_result *fr, va_list *ap);
--- ft_printf/ft_vprintf.c
+++ ft_printf/ft_vprintf.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 16:59:33 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/23 23:08:48 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/23 23:22:09 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,5 @@
 
 int	ft_vprintf(const char *format, va_list ap)
 {
-	t_format_result	fr;
-	t_format_info	fi;
-
-	fr = (t_format_result){format, 0};
-	while (*fr.format)
-	{
-		if (*fr.format == '%')
-		{
-			fi = (t_format_info){FLAG_NONE, WIDTH_NONE, PREC_NONE, LENGTH_NONE,
-				'\0'};
-			fp_configure_format_info(&fi, &fr, &ap);
-			fp_print_by_format(&fi, &fr, &ap);
-		}
-		else
-		{
-			ft_putchar_fd(*fr.format++, STDOUT_FILENO);
-			fr.cnt++;
-		}
-	}
-	return (fr.cnt);
+	return (ft_vdprintf(STDOUT_FILENO, format, ap));
 }
