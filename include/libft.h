f91b408e384c4b0b90a1255052ae4af6b1a68511
rask24 <rask0x18@gmail.com>
2024-02-24 13:20:46 +0900

    update libft header

--- include/libft.h
+++ include/libft.h
@@ -6,11 +6,20 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 14:54:41 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/24 12:47:00 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/24 13:20:31 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# include "ft_file.h"
+# include "ft_integer.h"
+# include "ft_memory.h"
+# include "ft_output.h"
+# include "ft_printf.h"
+# include "ft_string.h"
+# include "ft_type.h"
+# include "get_next_line.h"
 
 #endif
