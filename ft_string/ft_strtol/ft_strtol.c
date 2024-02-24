00785b446213044707989100e9a6a9adb410cdd6
rask24 <rask0x18@gmail.com>
2024-02-24 16:25:02 +0900

    refacotr with guard

--- ft_string/ft_strtol/ft_strtol.c
+++ ft_string/ft_strtol/ft_strtol.c
@@ -6,14 +6,14 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 16:42:38 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/24 16:09:58 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/24 16:24:29 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_string.h"
 #include "ft_type.h"
 
-static int	parse_prefix(t_tol_info *info)
+static int	_parse_prefix(t_tol_info *info)
 {
 	ptrdiff_t	index;
 	char		*ptr;
@@ -39,35 +39,47 @@ static int	parse_prefix(t_tol_info *info)
 	return (1);
 }
 
-static void	convert_str_to_long(t_tol_info *info)
+static void	_convert_str_to_long(t_tol_info *in)
 {
 	ptrdiff_t	index;
 	char		*ptr;
 
-	if (info->base == 16 && !parse_prefix(info))
+	if (in->base == 16 && !_parse_prefix(in))
 		return ;
 	while (1)
 	{
 		index = -1;
-		ptr = ft_strchr(UPPER_BASE, *info->str);
+		ptr = ft_strchr(UPPER_BASE, *in->str);
 		if (ptr)
 			index = ptr - UPPER_BASE;
-		ptr = ft_strchr(LOWER_BASE, *info->str);
+		ptr = ft_strchr(LOWER_BASE, *in->str);
 		if (ptr)
 			index = ptr - LOWER_BASE;
-		if (index < 0 || info->base <= index)
+		if (index < 0 || in->base <= index)
 			break ;
-		if (info->sign * info->nb > (LONG_MAX - index) / info->base && !info->overflow)
-			info->overflow = MAX_OVERFLOW;
-		if (info->sign * info->nb < (LONG_MIN + index) / info->base && !info->overflow)
-			info->overflow = MIN_OVERFLOW;
-		info->nb = info->nb * info->base + index;
-		info->digit_cnt++;
-		info->str++;
+		if (in->sign * in->nb > (LONG_MAX - index) / in->base && !in->overflow)
+			in->overflow = MAX_OVERFLOW;
+		if (in->sign * in->nb < (LONG_MIN + index) / in->base && !in->overflow)
+			in->overflow = MIN_OVERFLOW;
+		in->nb = in->nb * in->base + index;
+		in->digit_cnt++;
+		in->str++;
 	}
 }
 
-static int	init_conv_spec(t_tol_info *info,
+static void	_set_base(t_tol_info *info, int base)
+{
+	if (base)
+		info->base = base;
+	else if (*info->str == '0' && (info->str[1] == 'x' || info->str[1] == 'X'))
+		info->base = 16;
+	else if (*info->str == '0')
+		info->base = 8;
+	else
+		info->base = 10;
+}
+
+static void	_configure_tol_info(t_tol_info *info,
 				const char *str, char **endptr, int base)
 {
 	info->str = str;
@@ -78,30 +90,27 @@ static int	init_conv_spec(t_tol_info *info,
 	info->sign = 1;
 	info->digit_cnt = 0;
 	info->overflow = 0b00;
-	if (!(base == 0 || (2 <= base && base <= 36)))
-		return (0);
 	while (ft_isspace(*info->str))
 		info->str++;
 	if (*info->str == '+' || *info->str == '-')
+	{
 		if (*info->str++ == '-')
 			info->sign *= -1;
-	if (base)
-		info->base = base;
-	else if (*info->str == '0' && (info->str[1] == 'x' || info->str[1] == 'X'))
-		info->base = 16;
-	else if (*info->str == '0')
-		info->base = 8;
-	else
-		info->base = 10;
-	return (1);
+	}
+	_set_base(info, base);
 }
 
 long	ft_strtol(const char *str, char **endptr, int base)
 {
 	t_tol_info	info;
 
-	if (init_conv_spec(&info, str, endptr, base))
-		convert_str_to_long(&info);
+	if (!(base == 0 || (2 <= base && base <= 36)))
+	{
+		errno = EINVAL;
+		return (0);
+	}
+	_configure_tol_info(&info, str, endptr, base);
+	_convert_str_to_long(&info);
 	if (info.endptr && info.digit_cnt)
 		*info.endptr = (char *)(info.str);
 	if (info.overflow)
