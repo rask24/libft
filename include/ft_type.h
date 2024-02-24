28eedd7a5ec17710bd9d5f0c07138536ddd33a44
rask24 <rask0x18@gmail.com>
2024-02-24 16:26:20 +0900

    fix norm

--- include/ft_type.h
+++ include/ft_type.h
@@ -6,20 +6,20 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 12:44:55 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/24 16:08:32 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/24 16:26:09 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_TYPE_H
-#define FT_TYPE_H
+# define FT_TYPE_H
 
-int ft_isalnum(int c);
-int ft_isalpha(int c);
-int ft_isascii(int c);
-int ft_isdigit(int c);
-int ft_isprint(int c);
-int ft_isspace(int c);
-int ft_tolower(int c);
-int ft_toupper(int c);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+int	ft_isspace(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
 
 #endif
