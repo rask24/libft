dffc1118124d4861940b1a3407ddaf0a8648c98d
rask24 <rask0x18@gmail.com>
2024-06-20 23:38:11 +0900

    fix header format

--- include/ft_list.h
+++ include/ft_list.h
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 12:13:13 by reasuke           #+#    #+#             */
-/*   Updated: 2024/03/30 15:20:22 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/06/20 23:36:58 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,15 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-void				ft_lstadd_back(t_list **lst, t_list *new_lst);
-void				ft_lstadd_front(t_list **lst, t_list *new_lst);
-t_list				*ft_lst_before(t_list *lst, t_list *trg);
-void				ft_lstclear(t_list **lst, void (*del)(void *));
-void				ft_lstdelone(t_list *lst, void (*f)(void *));
-void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list				*ft_lstlast(t_list *lst);
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
-						void (*del)(void *));
-t_list				*ft_lstnew(void *content);
-int					ft_lstsize(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new_lst);
+void	ft_lstadd_front(t_list **lst, t_list *new_lst);
+t_list	*ft_lst_before(t_list *lst, t_list *trg);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*f)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 
 #endif
--- include/ft_string.h
+++ include/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 12:28:56 by reasuke           #+#    #+#             */
-/*   Updated: 2024/06/20 23:34:49 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/06/20 23:36:10 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,27 @@
 
 # include <stddef.h>
 
-int					ft_atoi(const char *str);
-char				*ft_basename(char *pathname);
-void				ft_free_strs(char **strs);
-char				*ft_itoa(int n);
-char				**ft_split(const char *s, char c);
-char				*ft_strchr(const char *s, int c);
-int					ft_strcmp(const char *s1, const char *s2);
-char				*ft_strdup(const char *s1);
-void				ft_striteri(char *s, void (*f)(unsigned int, char *));
-char				*ft_strjoin(const char *s1, const char *s2);
-size_t				ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t				ft_strlen(const char *s);
-char				*ft_strmapi(const char *s, char (*f)(unsigned int, char));
-int					ft_strncmp(const char *s1, const char *s2, size_t n);
-char				*ft_strndup(const char *s1, size_t n);
-char				*ft_strnstr(const char *haystack, const char *needle,
-						size_t len);
-char				*ft_strrchr(const char *s, int c);
-long				ft_strtol(const char *str, char **endptr, int base);
-char				*ft_strstr(const char *haystack, const char *needle);
-char				*ft_strtrim(const char *s1, const char *set);
-char				*ft_substr(const char *s, unsigned int start, size_t len);
+int		ft_atoi(const char *str);
+char	*ft_basename(char *pathname);
+void	ft_free_strs(char **strs);
+char	*ft_itoa(int n);
+char	**ft_split(const char *s, char c);
+char	*ft_strchr(const char *s, int c);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strdup(const char *s1);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strndup(const char *s1, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+long	ft_strtol(const char *str, char **endptr, int base);
+char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strtrim(const char *s1, const char *set);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 
 #endif
